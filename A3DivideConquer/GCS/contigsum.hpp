fb344ff11be14d1fdeffcf58578f65c1764b5eb2
llundell <llundell@ymail.com>
2019-10-09 20:29:52 -0800

    Initial commit

---
+++ A3DivideConquer/GCS/contigsum.hpp
@@ -0,0 +1,22 @@
+// Laura Lundell
+// Created 2019-10-09
+// Modified 2019-10-09
+// CS411 Analysis of Algorithms
+// Assignment 3-A: Greatest Contiguous Sum
+
+#ifndef contigsum_h
+#define contigsum_h
+struct Video {
+  int A_seqGCS;
+  int B_firstValGCS;
+  int C_lastValGCS;
+  int D_entireSeqSum;
+};
+
+template<typename RAIter>
+int contigSum(RAIter first, RAIter last)
+{
+  return 0;
+}
+
+#endif /* contigsum_h */
