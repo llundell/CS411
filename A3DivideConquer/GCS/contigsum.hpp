10becd48a7d3fcf57777a9c8ee5adc9ccb598232
llundell <llundell@ymail.com>
2019-10-15 21:58:18 -0800

    Compiles, passes 4 tests

--- A3DivideConquer/GCS/contigsum.hpp
+++ A3DivideConquer/GCS/contigsum.hpp
@@ -1,21 +1,35 @@
 // Laura Lundell
 // Created 2019-10-09
-// Modified 2019-10-09
+// Modified 2019-10-15
 // CS411 Analysis of Algorithms
 // Assignment 3-A: Greatest Contiguous Sum
 
 #ifndef contigsum_h
 #define contigsum_h
-struct Video {
+
+#include <iterator>
+using std::distance; //compute size
+
+struct GCSValues
+{
   int A_seqGCS;
   int B_firstValGCS;
   int C_lastValGCS;
   int D_entireSeqSum;
 };
 
+// template<typename RAIter>
+// GCSValues divideConquer(RAIter first, RAIter last)
+// {
+//
+//
+//
+// }
+
 template<typename RAIter>
 int contigSum(RAIter first, RAIter last)
 {
+  int seqSize = distance(first, last);
   return 0;
 }
 
